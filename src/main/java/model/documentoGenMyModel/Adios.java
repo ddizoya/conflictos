64698f693baccc7348e651ef9d4defb3f359522f
ddizoya <ddizoya@eniac30.danielcastelao.org>
2015-01-29 08:56:15 +0100

    Commit inicial.

---
+++ src/main/java/model/documentoGenMyModel/Adios.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.documentoGenMyModel;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ddizoya
+ */
+public class Adios {
+    public void Chao(){
+        JOptionPane.showMessageDialog(null, "JEJEJEJEJEJE CHAO");
+    }
+    
+}
---
+++ src/main/java/model/documentoGenMyModel/Hola.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.documentoGenMyModel;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ddizoya
+ */
+public class Hola {
+       
+    public static void ver(){
+        JOptionPane.showConfirmDialog(null, "Esto es una prueba. ");
+    }
+    
+    public static void Damian(){
+        JOptionPane.showConfirmDialog(null, "jeje");
+    }
+}
